c2a8afe58aee7e040a5134b56d5d09eb58ada3a9
joszeto1 <joszeto1@s.sfusd.edu>
2024-05-13 14:33:21 -0700

    player health bar that takes damage

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal health_depleted
+
 var health = 100.0
 
 func _physics_process(delta):
@@ -12,4 +14,13 @@ func _physics_process(delta):
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
+		
+	const DAMAGE_RATE = 10.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
 	
